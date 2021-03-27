4d178c3aec2b3b398cd34d15f00f194e5115e7fd
zakariyaeELKHALDI <81492191+zakariyaeELKHALDI@users.noreply.github.com>
2021-03-27 23:52:57 +0100

    Update iterative_fibo.c

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -2,15 +2,12 @@
 
 int iterative_fibo(int n)
 {   
-    int i=2;
-    while i<=n
+    for (int i=2; i<=n; ++i)
     {
         int F0= 0, F1= 1, F= 0;
             F= F1 + F0;
             F0= F1;
             F1= F;
-            i+=1;
-            iterative_fibo(i);
     }
     return F;
 }
