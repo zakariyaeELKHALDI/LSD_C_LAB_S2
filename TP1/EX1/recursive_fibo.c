c433312f609a4dba8aa017223326d67a8b9ab9f7
zakariyaeELKHALDI <81492191+zakariyaeELKHALDI@users.noreply.github.com>
2021-03-27 23:50:47 +0100

    Update recursive_fibo.c

--- TP1/EX1/recursive_fibo.c
+++ TP1/EX1/recursive_fibo.c
@@ -1,6 +1,16 @@
 #include "header.h"
 
 int recursive_fibo(int n)
-{
-    // write your recursive fibo code here
+{   
+    int i=2;
+    while i<=n
+    {
+        int F0= 0, F1= 1, F= 0;
+            F= F1 + F0;
+            F0= F1;
+            F1= F;
+            i+=1;
+            recursive_fibo(i);
+    }
+    return F;
 }
