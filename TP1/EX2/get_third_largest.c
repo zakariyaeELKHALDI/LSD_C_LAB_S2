d78f15638125cecf1052d90a4e235104db3b3b52
zakariyaeELKHALDI <81492191+zakariyaeELKHALDI@users.noreply.github.com>
2021-03-28 00:03:38 +0100

    Update get_third_largest.c

--- TP1/EX2/get_third_largest.c
+++ TP1/EX2/get_third_largest.c
@@ -7,5 +7,24 @@
 */
 int     get_third_largest(int *tab, int size)
 {
-        // write your algo here
+        int first, second, third;
+        first= 0;
+        second= 0;
+        third= 0;
+        for (i=0;i<size;i++){
+     	        if (tab[i]>first)
+     		        first=tab[i];
+        }
+	for (i=0;i<size;i++){
+     	        if (tab[i]>second && tabe[i]<first)
+     		        second=tab[i];
+        }
+        for (i=0;i<size;i++){ 
+     	        if (tab[i]>third && tab[i]<second)
+     		        third=tab[i];
+        }
+        printf("first= %d\n", first);
+        printf("second= %d\n", second);
+        printf("third= %d\n", third);
+        return third;
 }
